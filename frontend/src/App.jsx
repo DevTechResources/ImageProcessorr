c355ad15131870d472a9f189a85aca1420045d4a
DevTechResources <genericosistem@gmail.com>
2025-09-08 13:33:50 -0500

    HelpPage.jsx
    Modificación de la pagina de ayuda.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import LandingPage from './components/LandingPage';
-import ImageProcessor from './components/ImageProcessor';
 import HelpPage from './components/HelpPage';
 import './App.css';
 
